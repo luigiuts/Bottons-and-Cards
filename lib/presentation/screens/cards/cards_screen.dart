ff849fd96de9e618d0b0907c00710095606316ef
Luigi Estevez <lsestevez@uts.edu.co>
2024-07-11 18:41:09 -0500

    rutas basicas configuradas

---
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class CardsSreen extends StatelessWidget {
+  const CardsSreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cards Screen'),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -39,17 +40,23 @@ class _CustomListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-  final colors = Theme.of(context).colorScheme;
+    final colors = Theme.of(context).colorScheme;
 
     return ListTile(
-      leading: Icon(menuItem.icon, color:colors.primary),
+      leading: Icon(menuItem.icon, color: colors.primary),
       trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
-      onTap: (){
-        //TODO: navegar a otra pantalla
-      },
+      onTap: () {
+
+      //   Navigator.of(context).push(
+      //     MaterialPageRoute(
+      //       builder: (context) => const ButtonsScreen(),
+      //     ),
+      //   );
+      Navigator.pushNamed(context, menuItem.link);
+       },
+
     );
   }
 }
